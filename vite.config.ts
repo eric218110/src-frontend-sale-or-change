ebe7ffba121407f3944a1a019df50d63a68cb5d1
Eric Silva <ericsilvaccp@gmail.com>
2023-02-08 22:28:54 -0300

    chore:  config code styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        quietDeps: true,
-      },
-    },
-  },
-  plugins: [react()],
-});
+  plugins: [svgr(), react(), tsconfigPaths()],
+  resolve: {
+    alias: [
+      {
+        find: '@presentation',
+        replacement: path.resolve('./src/presentation')
+      }
+    ]
+  }
+})
