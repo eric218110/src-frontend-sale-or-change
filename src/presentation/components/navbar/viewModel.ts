1ca4b864319dfcd8c58fcb9529532344178134ba
Eric Silva <ericsilvaccp@gmail.com>
2023-02-10 00:14:58 -0300

    create nav bar view model

--- src/presentation/components/navbar/viewModel.ts
+++ src/presentation/components/navbar/viewModel.ts
@@ -1,12 +1,25 @@
 import { paths } from '@presentation/routes/path'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { FaListUl } from 'react-icons/fa'
 import { HiOutlineMailOpen } from 'react-icons/hi'
 import { MdLibraryAdd } from 'react-icons/md'
+import { useLocation } from 'react-router-dom'
 import * as T from './types'
 
 export const useNavBarViewModel = () => {
   const [activeItem, setActiveItem] = useState<T.ItemType>(undefined)
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    Object.entries(paths).forEach(([_, value]) => {
+      if (pathname.includes(value)) {
+        if (value === paths.negotiations) {
+          setActiveItem('negotiation')
+        }
+      }
+    })
+  }, [pathname])
 
   const itemIsActive = (item: T.ItemType) => item === activeItem
 
